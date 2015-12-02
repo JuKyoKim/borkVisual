c5e933ae75f0af2a441aaf836f8b1811bf176043
jukyokim <jukyokim1992@gmail.com>
2015-12-02 16:35:57 -0500

    commiting initial route, controller, view set up

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,51 @@
+class UserController < ApplicationController
+	def index
+		@user = "asdfas"
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to @user
+		else
+			redirect_to root_path
+		end
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		@user.update
+		if @user.save
+			redirect_to @user
+		else
+			redirect_to root_path
+		end
+	end
+
+	def destroy
+		@user = User.find(params[:id]).destroy
+ 		redirect_to root_path
+	end
+
+
+
+	private
+
+ 	def user_params
+ 		params.require(:user).permit()
+ 	end
+
+
+end
